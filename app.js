8d08ecf9b34e2fe6ed29d20ca0e171d89b2d648a
Julia Robinson <juliaarobinson@hotmail.com>
2021-12-30 21:49:56 +0000

    basic app

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () =>
+  console.log(`Example app listening on port ${process.env.MAPS_API_KEY}!`)
+);

190345082d45569e33f19a82d496a85ac242923f
Julia Robinson <juliaarobinson@hotmail.com>
2022-01-01 20:26:16 +0000

    make google fetch request

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname));
+app.use(express.json({ limit: "1mb" }));
 app.listen(port, () => console.log(`Listening`));
 
-app.post("/api/map", function (req, res) {
-  res.send({
-    key: "secret key",
-  });
+app.post("/api/map", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.address},+${req.body.city}&key=${process.env.MAPS_API_KEY}`
+    );
+
+    const coords = await response.json();
+
+    res.send({
+      key: coords.results[0].geometry.location,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+const data = { address: "59 Love Lane", city: "Pinner" };
+
 fetch("/api/map", {
   method: "POST", // or 'PUT'
   headers: {
     "Content-Type": "application/json",
   },
-  // body: JSON.stringify(data),
+  body: JSON.stringify(data),
+  mode: "cors",
 })
   .then((response) => response.json())
   .then((response) => console.log(response.key));
